ccc30d19f4a873d15658b826732bb3ec42101b54
nemovim <nemovim@kentech.ac.kr>
2025-03-11 00:46:08 +0000

    Add prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+pip install -r requirements.txt
+pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
+# git clone https://github.com/NVlabs/nvdiffrast
+pip install ./nvdiffrast
+./start.sh
